dc9efd155305b7499009c15a7ab4d68f7a030ab0
fl4r30n <flareon_rojo@hotmail.com>
2024-04-30 09:16:51 -0600

    cambios archivos settigs y wsgi

--- portafolio/portafolio/wsgi.py
+++ portafolio/portafolio/wsgi.py
@@ -1,16 +1,11 @@
-"""
-WSGI config for portafolio project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
-"""
 
 import os
 
-from django.core.wsgi import get_wsgi_application
+# Determinar el entorno
+ENVIRONMENT = os.getenv('ENVIRONMENT', 'development')
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portafolio.settings')
-
-application = get_wsgi_application()
+# Importar las configuraciones adecuadas según el entorno
+if ENVIRONMENT == 'production':
+    from .wsgi_prod import *
+else:
+    from .wsgi_dev import *
