707de07bfa88e1f507fda20016559c9cd3e21226
fl4r30n <flareon_rojo@hotmail.com>
2024-09-14 15:47:02 -0600

    eliminada url about-me

--- portafolio/about_me/views.py
+++ portafolio/about_me/views.py
@@ -2,6 +2,3 @@ from django.shortcuts import render
 from .models import AboutMe
 
 # Create your views here.
-def about(request):
-    aboutme = AboutMe.objects.all()
-    return render(request,'about-me/about-me.html',{'aboutme':aboutme})
--- portafolio/portafolio/urls.py
+++ portafolio/portafolio/urls.py
@@ -18,11 +18,10 @@ from django.contrib import admin
 from django.urls import path
 from core import views as core_views
 from portfolio import views as portfolio_views
-from about_me import views as about_me_views
+
 
 urlpatterns = [
     path('',core_views.home, name='home'),
-    path('about-me/', about_me_views.about, name='about'),
     path('portfolio/', portfolio_views.portfolio, name='portfolio'),
     path(
         "portfolio-details/<int:pk>/",
